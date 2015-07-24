1f1bbc78886a931112cc47c2fd109135ba2c401e
Patrick Maughan <pfmaug@gmail.com>
2015-07-24 12:12:43 -0700

    Refactor memory init again
    More, er, bugs in memory initialization, this gets rid of all the "Worker1" references and makes the function truly reusable.

--- Live-Code/main.js
+++ Live-Code/main.js
@@ -10,23 +10,23 @@ You give orders to a creep by its name this way: Game.creeps.Worker1.
 
 Creep.prototype.WorkerCreepHarvestCycle = function () 
 {
-    if(typeof Game.creeps.Worker1.memory.mySource === 'undefined') Game.creeps.Worker1.memory.mySource=Game.creeps.Worker1.pos.findClosestByRange(FIND_SOURCES);
-    if(typeof Game.creeps.Worker1.memory.mySpawn  === 'undefined') Game.creeps.Worker1.memory.mySpawn =Game.creeps.Worker1.pos.findClosestByRange(FIND_MY_SPAWNS);
-    if(typeof Game.creeps.Worker1.memory.myDest   === 'undefined') Game.creeps.Worker1.memory.myDest  =Game.creeps.Worker1.memory.mySource;
+    if(typeof this.memory.mySource === 'undefined') this.memory.mySource=this.pos.findClosestByRange(FIND_SOURCES);
+    if(typeof this.memory.mySpawn  === 'undefined') this.memory.mySpawn =this.pos.findClosestByRange(FIND_MY_SPAWNS);
+    if(typeof this.memory.myDest   === 'undefined') this.memory.myDest  =this.memory.mySource;
     
     var myDest   = Game.getObjectById(this.memory.myDest.id);
     var mySource = Game.getObjectById(this.memory.mySource.id);
     var mySpawn  = Game.getObjectById(this.memory.mySpawn.id);
     
-    if(this.carry.energy == 0)
+    if(this.carry.energy == 0 && this.memory.myDest.id != mySource.id)
     {
-        console.log("Worker1 is off to harvest from source @" + mySource.pos + " -- ID " + mySource.id);
+        console.log(this.name + " is off to harvest from source @" + mySource.pos + " -- ID " + mySource.id);
         this.say("<< " + mySource.pos.x + ":" + mySource.pos.y);
         this.memory.myDest = mySource;
     }
-    else if(this.carry.energy == this.carryCapacity && this.memory.myDest != mySpawn)
+    else if(this.carry.energy == this.carryCapacity && this.memory.myDest.id != mySpawn.id)
     {
-        console.log("Worker1 is off to transfer energy to spawn @" + mySpawn.pos + " -- ID " + mySpawn.id);
+        console.log(this.name + " is off to transfer energy to spawn @" + mySpawn.pos + " -- ID " + mySpawn.id);
         this.say("<< " + mySpawn.pos.x + ":" + mySpawn.pos.y);
         this.memory.myDest = mySpawn;
     }
@@ -40,5 +40,5 @@ Creep.prototype.WorkerCreepHarvestCycle = function ()
 if(typeof Game.creeps.Worker1 === 'undefined') Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Worker1' );
 else Game.creeps.Worker1.WorkerCreepHarvestCycle();
 
-if(typeof Game.creeps.Worker2 === 'undefined') Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Worker1' );
+if(typeof Game.creeps.Worker2 === 'undefined') Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Worker2' );
 else Game.creeps.Worker2.WorkerCreepHarvestCycle();
